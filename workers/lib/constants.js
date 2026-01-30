1d301086d30d84273870dda1c3f856f2181e8be8
Eugene Glova <eugene.glova@gmail.com>
2026-01-30 19:37:29 +0300

    add constants

--- workers/lib/constants.js
+++ workers/lib/constants.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const BTC_SATS = 100000000 // satoshis in 1 btc
+const MINUTE_MS = 60 * 1000
 const HOUR_MS = 60 * 60 * 1000
 const HOURS_24_MS = 24 * 60 * 60 * 1000
 const POOL_TYPE = 'ocean'
@@ -13,6 +14,7 @@ const SCHEDULER_TIMES = {
 
 module.exports = {
   BTC_SATS,
+  MINUTE_MS,
   HOUR_MS,
   HOURS_24_MS,
   SCHEDULER_TIMES,
